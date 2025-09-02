63a221c4f0fa702de4c423ccd0587ff1ee84e3c9
Janne Vanhauwaert <janne.vanhauwaert@gmail.com>
2025-09-02 21:28:11 +0200

    Setup client

---
+++ client/src/app/layout/header/header.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button'
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    MatIcon,
+    MatButton,
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './header.html',
+  styleUrl: './header.scss'
+})
+export class Header {
+
+}
