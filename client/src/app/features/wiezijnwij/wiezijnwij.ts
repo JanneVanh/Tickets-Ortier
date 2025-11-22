2590c8cd5d5b508ca5af52114a6dba962893b3cd
Janne Vanhauwaert <janne.vanhauwaert@gmail.com>
2025-11-22 11:11:42 +0100

    Wie zijn wij responsive

--- client/src/app/features/wiezijnwij/wiezijnwij.ts
+++ client/src/app/features/wiezijnwij/wiezijnwij.ts
@@ -1,32 +1,36 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { MatIcon } from '@angular/material/icon';
 import { ReservationService } from '../../core/services/reservation';
 
 @Component({
   selector: 'app-wiezijnwij',
   imports: [
-    MatIcon
   ],
   templateUrl: './wiezijnwij.html',
   styleUrl: './wiezijnwij.scss'
 })
-export class Wiezijnwij implements OnInit{
+export class Wiezijnwij implements OnInit {
   private reservationService = inject(ReservationService)
 
   ngOnInit(): void {
     this.reservationService.emptyReservation();
   }
-  
+
   sopranen = {
     description: `Lore, Sofie, Katrien, Nancy, Anniek, Lynn, Marijke, Karlien, Zyncke, Tine, Sharon, Ann, Lieve`
-  };  
+  };
   alten = {
     description: `Alten in volgorde van foto`
-  };  
+  };
   tenoren = {
     description: `Frederik, Matty, Philippe, Cris, Henk, Pieter, Bart`
-  };  
+  };
   bassen = {
     description: `Bassen in volgorde van foto`
+  };
+
+  choirInfo = {
+    description: `
+    Simpelweg voor 't plezier van het zingen komen we iedere maandagavond samen om ... te zingen. </br>
+    Al 20 jaar lang. </br>`
   };
 }
