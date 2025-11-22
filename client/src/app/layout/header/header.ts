b13362b6c3ff3defb9153a20c7655eea51c394d5
Janne Vanhauwaert <janne.vanhauwaert@gmail.com>
2025-11-22 09:39:36 +0100

    make header responsive

--- client/src/app/layout/header/header.ts
+++ client/src/app/layout/header/header.ts
@@ -1,9 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
-import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { Account } from '../../core/services/account';
+import { Title } from '@angular/platform-browser';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -19,13 +21,50 @@ import { Account } from '../../core/services/account';
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
-export class Header {
+export class Header implements OnInit {
   accountService = inject(Account)
   router = inject(Router)
+  titleService = inject(Title)
+  mobileMenuOpen = false;
+  pageTitle = '';
+  
+  ngOnInit() {
+    this.updatePageTitle();
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.updatePageTitle();
+    });
+  }
+  
+  updatePageTitle() {
+    const route = this.router.url;
+    if (route === '/' || route.startsWith('/?')) {
+      this.pageTitle = 'Home';
+    } else if (route.startsWith('/wiezijnwij')) {
+      this.pageTitle = 'Wie zijn wij?';
+    } else if (route.startsWith('/ticketinfo')) {
+      this.pageTitle = 'Tickets';
+    } else if (route.startsWith('/reservations')) {
+      this.pageTitle = 'Reservations';
+    } else if (route.startsWith('/tickets')) {
+      this.pageTitle = 'Tickets';
+    } else {
+      this.pageTitle = '';
+    }
+  }
   
   get canAccessReservations(): boolean {
     const user = this.accountService.currentUser();
     return user !== null && user.roles && user.roles.includes('Admin');
+  }
+  
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+  
+  closeMobileMenu() {
+    this.mobileMenuOpen = false;
   }
   
   logout(){
